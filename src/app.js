06005f80ad6cb514c7c01758c37887d57e171c40
Byron Jimenez <byronjimenez9911@gmail.com>
2024-04-30 14:39:31 -0600

    Install and setup cors

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const { notFound, errorHandler } = require('./middlewares');
 
@@ -9,13 +10,16 @@ const app = express();
 
 require('dotenv').config();
 
+app.use(cors({ origin: '*' }));
 app.use(helmet());
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
 const authRoutes = require('./routes/auth');
+const qrRoutes = require('./routes/qr');
 
 app.use('/api/auth', authRoutes);
+app.use('/api/qr', qrRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
