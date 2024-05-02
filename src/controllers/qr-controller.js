ec9ebe07117aedcc21fabd23327b0df98fcb02a4
Byron Jimenez <byronjimenez9911@gmail.com>
2024-05-02 10:33:45 -0600

    Update qr queries

--- src/controllers/qr-controller.js
+++ src/controllers/qr-controller.js
@@ -1,7 +1,9 @@
 const db = require('../config/db')
 
 const validate = async (req, res, next) => {
-  const { hash } = req.body;
+  const { hash, detail } = req.query;
+
+  console.log(hash, detail);
 
   if (!hash) {
     return res.status(400).json({ message: 'QR hash is required' });
@@ -10,13 +12,21 @@ const validate = async (req, res, next) => {
   const request = await db.request();
 
   // query to the database
-  const result = await request.query`SELECT * FROM qr WHERE qr = ${qr}`;
+  const result = await request.query`SELECT * FROM qrs WHERE hash = ${hash}`;
 
   if (result.recordset.length === 0) {
-    return res.status(404).json({ message: 'QR not found' });
+    return res.status(404).json({ message: 'El codigo no es valido' });
   }
 
-  return res.status(200).json({ message: 'QR found' });
+  if (detail && detail === 'true') {
+    return res.status(200).json(result.recordset[0]);
+  }
+
+  const request2 = await db.request();
+
+  await request2.query`UPDATE qrs SET verified_at = ${new Date()} WHERE hash = ${hash}`;
+
+  return res.status(200).json({ message: 'QR valido' });
 }
 
 module.exports = {
