6ea2cf5c3bfddeef24aa2dea083f1ebb2bf5098d
Byron Jimenez <byronjimenez9911@gmail.com>
2024-05-03 11:24:05 -0600

    Add https support

--- src/server.js
+++ src/server.js
@@ -1,6 +1,15 @@
 const app = require('./app');
+const fs = require('fs')
+const https = require("https")
+const http = require("http")
 
 const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+
+const server = process.env.NODE_ENV === 'production' ? https.createServer({
+  key: fs.readFileSync(process.env.SSL_KEY_PATH),
+  cert: fs.readFileSync(process.env.SSL_CERT_PATH)
+}, app) : http.createServer(app)
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
